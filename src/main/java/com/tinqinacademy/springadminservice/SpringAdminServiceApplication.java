4990d980f705c346f7394bceb6eeb31da6d87c3c
Diyan Dimitrov <dimitrovdian330@gmail.com>
2024-08-07 10:14:00 +0300

    Initial commit

---
+++ src/main/java/com/tinqinacademy/springadminservice/SpringAdminServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tinqinacademy.springadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminServiceApplication.class, args);
+    }
+
+}
